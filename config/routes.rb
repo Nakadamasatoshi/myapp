7af84770d9da4d2ddd03847c0363990bc0ee193a
Nakadamasatoshi <miyabi_0430@yahoo.co.jp>
2023-08-20 19:50:17 +0900

    NoMethodError

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #top_controller
+  get '/', to: 'top#index', as: 'index_top'
 end
